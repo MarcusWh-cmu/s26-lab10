67b043be510342d2f0fd4a2728d970300c5ee737
marcuswh <marcuswh@andrew.cmu.edu>
2026-03-27 13:00:31 -0400

    Tasks 1 and 3 Complete

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,50 +1,44 @@
 import React, { useState } from 'react'
 import './Quiz.css'
 import QuizQuestion from '../core/QuizQuestion';
+import QuizCore from '../core/QuizCore'
 // Hint: Take advantage of the QuizQuestion interface
-
-interface QuizState {
-  questions: QuizQuestion[]
-  currentQuestionIndex: number
-  selectedAnswer: string | null
-  score: number
-}
 
 const Quiz: React.FC = () => {
   // TODO: Task1 - Seprate the logic of quiz from the UI.
   // Hint: Take advantage of QuizCore to manage quiz state separately from the UI.
-  const initialQuestions: QuizQuestion[] = [
-    {
-      question: 'What is the capital of France?',
-      options: ['London', 'Berlin', 'Paris', 'Madrid'],
-      correctAnswer: 'Paris',
-    },
-  ];
-  const [state, setState] = useState<QuizState>({
-    questions: initialQuestions,
-    currentQuestionIndex: 0,  // Initialize the current question index.
-    selectedAnswer: null,  // Initialize the selected answer.
-    score: 0,  // Initialize the score.
-  });
+  const [quiz] = useState(() => new QuizCore())
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null)
+  const [currentQuestion, setCurrentQuestion] = useState<QuizQuestion | null>(quiz.getCurrentQuestion())
+  const [score, setScore] = useState<number>(quiz.getScore())
+
 
   const handleOptionSelect = (option: string): void => {
-    setState((prevState) => ({ ...prevState, selectedAnswer: option }));
+    setSelectedAnswer(option)
   }
 
 
   const handleButtonClick = (): void => {
     // TODO: Task3 - Implement the logic for button click ("Next Question" and "Submit").
     // Hint: You might want to check for a function in the core logic to help with this.
+    if(!selectedAnswer || !currentQuestion) {
+      return
+    }
+
+    quiz.answerQuestion(selectedAnswer)
+    quiz.nextQuestion()
+
+    setCurrentQuestion(quiz.getCurrentQuestion())
+    setScore(quiz.getScore())
+    setSelectedAnswer(null)
   } 
 
-  const { questions, currentQuestionIndex, selectedAnswer, score } = state;
-  const currentQuestion = questions[currentQuestionIndex];
 
   if (!currentQuestion) {
     return (
       <div>
         <h2>Quiz Completed</h2>
-        <p>Final Score: {score} out of {questions.length}</p>
+        <p>Final Score: {score} out of {quiz.getTotalQuestions()}</p>
       </div>
     );
   }
